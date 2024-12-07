50786e0aab06b12b681184763a44c9af386e2421
wilsonify <tom.andrew.wilson@gmail.com>
2024-12-07 16:55:59 -0500

    page.php

--- Code Download/c02 - Laying Out the Foundations/Code/presentation/page.php
+++ Code Download/c02 - Laying Out the Foundations/Code/presentation/page.php
@@ -1,21 +1,21 @@
 <?php
-// Reference Smarty library
-require_once SMARTY_DIR . 'Smarty.class.php';
+// Reference Composer's autoload
+require_once '/var/www/html/vendor/autoload.php';
 
 /* Class that extends Smarty, used to process and display Smarty
    files */
-class Page extends Smarty
+class Page extends \Smarty\Smarty
 {
-  // Class constructor
-  public function __construct()
-  {
-    // Call Smarty's constructor
-    parent::Smarty();
+    // Class constructor
+    public function __construct()
+    {
+        // Call Smarty's constructor (without using Smarty())
+        parent::__construct();  // This is the correct constructor call
 
-    // Change the default template directories
-    $this->template_dir = TEMPLATE_DIR;
-    $this->compile_dir = COMPILE_DIR;
-    $this->config_dir = CONFIG_DIR;
-  }
+        // Change the default template directories
+        $this->template_dir = TEMPLATE_DIR;
+        $this->compile_dir = COMPILE_DIR;
+        $this->config_dir = CONFIG_DIR;
+    }
 }
 ?>
