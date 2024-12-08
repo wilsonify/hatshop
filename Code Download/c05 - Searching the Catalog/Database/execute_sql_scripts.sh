f2c1caf5ea90294bf70cd140a79542ea32e3bf9f
wilsonify <tom.andrew.wilson@gmail.com>
2024-12-08 16:03:12 -0500

    execute_sql_scripts.sh

--- Code Download/c05 - Searching the Catalog/Database/execute_sql_scripts.sh
+++ Code Download/c05 - Searching the Catalog/Database/execute_sql_scripts.sh
@@ -2,9 +2,6 @@
 set -euxo pipefail
 DB_PORT="5432"
 
-echo "Executing c0401-create-category-table.sql..."
-psql -U "$HATSHOP_DB_USERNAME" -h "$HATSHOP_DB_SERVER" -p "$DB_PORT" -d "$HATSHOP_DB_DATABASE" -f "c0401-create-category-table.sql"
-echo "Done with c0401-create-category-table.sql"
 
 echo "Executing c0501-alter-product-table-adding-search_vector-field.sql"
 psql -U "$HATSHOP_DB_USERNAME" -h "$HATSHOP_DB_SERVER" -p "$DB_PORT" -d "$HATSHOP_DB_DATABASE" -f "c0501-alter-product-table-adding-search_vector-field.sql"
