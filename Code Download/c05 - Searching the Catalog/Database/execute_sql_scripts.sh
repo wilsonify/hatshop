3368acf9cd2e098953b6c6aef6425fcc7cabb4a1
wilsonify <tom.andrew.wilson@gmail.com>
2024-12-08 15:51:07 -0500

    c0501-alter-product-table adding search_vector field.sql c0502-create-index-for search_vector field in product table.sql c0503-update-newly-added search_vector field from product table.sql c0504-create-catalog_flag_stop_words-function.sql c0505-function-catalog_count_search_result.sql c0506-create-catalog_search-function.sql

--- Code Download/c05 - Searching the Catalog/Database/execute_sql_scripts.sh
+++ Code Download/c05 - Searching the Catalog/Database/execute_sql_scripts.sh
@@ -6,5 +6,4 @@ echo "Executing c0401-create-category-table.sql..."
 psql -U "$HATSHOP_DB_USERNAME" -h "$HATSHOP_DB_SERVER" -p "$DB_PORT" -d "$HATSHOP_DB_DATABASE" -f "c0401-create-category-table.sql"
 echo "Done with c0401-create-category-table.sql"
 
-
 echo "All SQL scripts executed successfully!"
