28ade434b253e680166f7620e3614bc8673498b2
wilsonify <tom.andrew.wilson@gmail.com>
2024-12-08 10:31:39 -0500

    page.php

--- Code Download/c04 - Creating the Product Catalog Part II/Code/presentation/page.php
+++ Code Download/c04 - Creating the Product Catalog Part II/Code/presentation/page.php
@@ -1,6 +1,15 @@
 <?php
 // Reference Smarty library
-require_once SMARTY_DIR . 'Smarty.class.php';
+require_once '/var/www/html/vendor/autoload.php';
+
+require_once __DIR__ . '/smarty_plugins/function.load_categories_list.php';
+require_once __DIR__ . '/smarty_plugins/function.load_department.php';
+require_once __DIR__ . '/smarty_plugins/function.load_departments_list.php';
+require_once __DIR__ . '/smarty_plugins/function.load_product.php';
+require_once __DIR__ . '/smarty_plugins/function.load_products_list.php';
+require_once __DIR__ . '/smarty_plugins/modifier.prepare_link.php';
+
+use Smarty\Smarty;
 
 /* Class that extends Smarty, used to process and display Smarty
    files */
@@ -10,14 +19,20 @@ class Page extends Smarty
   public function __construct()
   {
     // Call Smarty's constructor
-    parent::Smarty();
+    parent::__construct();
 
     // Change the default template directories
     $this->template_dir = TEMPLATE_DIR;
     $this->compile_dir = COMPILE_DIR;
     $this->config_dir = CONFIG_DIR;
-    $this->plugins_dir[0] = SMARTY_DIR . 'plugins';
-    $this->plugins_dir[1] = PRESENTATION_DIR . 'smarty_plugins';
+
+    $this->registerPlugin('modifier', 'smarty_modifier_prepare_link', 'smarty_function_load_categories_list');
+    $this->registerPlugin('function', 'load_categories_list', 'smarty_function_load_categories_list');
+    $this->registerPlugin('function', 'load_department', 'smarty_function_load_department');
+    $this->registerPlugin('function', 'load_departments_list', 'smarty_function_load_departments_list');
+    $this->registerPlugin('function', 'load_product', 'smarty_function_load_product');
+    $this->registerPlugin('function', 'load_products_list', 'smarty_function_load_products_list');
+    $this->registerPlugin('modifier', 'prepare_link', 'smarty_modifier_prepare_link');
   }
 }
 ?>
