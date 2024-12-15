cb97e5846f59c09a6c0ba58baf06614c24248397
wilsonify <tom.andrew.wilson@gmail.com>
2024-12-15 08:34:50 -0500

    config.php

--- Code Download/c17 - Connecting to Web Services/Code/Datacash/include/config.php
+++ Code Download/c17 - Connecting to Web Services/Code/Datacash/include/config.php
@@ -12,23 +12,23 @@ define('TEMPLATE_DIR', PRESENTATION_DIR . '/templates');
 define('COMPILE_DIR', PRESENTATION_DIR . '/templates_c');
 define('CONFIG_DIR', SITE_ROOT . '/include/configs');
 
-// These should be true while developing the web site
-define('IS_WARNING_FATAL', true);
-define('DEBUGGING', true);
+// Settings for developer exerience
+define('IS_WARNING_FATAL', getenv('HATSHOP_IS_WARNING_FATAL',true));
+define('DEBUGGING', getenv('HATSHOP_DEBUGGING',true));
 
 // The error types to be reported
 define('ERROR_TYPES', E_ALL);
 
 // Settings about mailing the error messages to admin
-define('SEND_ERROR_MAIL', false);
-define('ADMIN_ERROR_MAIL', 'admin@example.com');
-define('SENDMAIL_FROM', 'errors@example.com');
+define('SEND_ERROR_MAIL', getenv('HATSHOP_SEND_ERROR_MAIL',false));
+define('ADMIN_ERROR_MAIL', getenv('HATSHOP_ADMIN_ERROR_MAIL','admin@example.com'));
+define('SENDMAIL_FROM', getenv('HATSHOP_SENDMAIL_FROM','errors@example.com'));
 ini_set('sendmail_from', SENDMAIL_FROM);
 
 // By default we don't log errors to a file
-define('LOG_ERRORS', false);
-define('LOG_ERRORS_FILE', 'c:\\hatshop\\errors_log.txt'); // Windows
-// define('LOG_ERRORS_FILE', '/var/tmp/hatshop_errors.log'); // Unix
+define('LOG_ERRORS', getenv('HATSHOP_LOG_ERRORS', false));
+define('LOG_ERRORS_FILE', getenv('HATSHOP_LOG_ERRORS_FILE', '/var/tmp/hatshop_errors.log'));
+
 
 /* Generic error message to be displayed instead of debug info
    (when DEBUGGING is false) */
