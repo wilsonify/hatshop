e581e0367359aab1ce0a157350b08cbcd4b56e04
wilsonify <tom.andrew.wilson@gmail.com>
2024-12-31 16:24:37 -0500

    SmartyModifierPrepareLinkTest.php

--- c04 - Creating the Product Catalog Part II/Code/tests/SmartyModifierPrepareLinkTest.php
+++ c04 - Creating the Product Catalog Part II/Code/tests/SmartyModifierPrepareLinkTest.php
@@ -1,6 +1,8 @@
 <?php
 
+require_once __DIR__ . '/../presentation/smarty_plugins/modifier.prepare_link.php';
 use PHPUnit\Framework\TestCase;
+
 
 class SmartyModifierPrepareLinkTest extends TestCase
 {
@@ -12,35 +14,35 @@ class SmartyModifierPrepareLinkTest extends TestCase
     public function testPrepareLinkWithIndexPhp()
     {
         $result = smarty_modifier_prepare_link('index.php');
-        $expected = 'https://testserver.com/index.php'
+        $expected = 'https://testserver.com/index.php';
         $this->assertEquals($expected, $result);
     }
 
     public function testPrepareLinkWithAdminPhp()
     {
         $result = smarty_modifier_prepare_link('admin.php');
-        $expected = 'https://testserver.com/admin.php'
+        $expected = 'https://testserver.com/admin.php';
         $this->assertEquals($expected, $result);
     }
 
     public function testPrepareLinkWithoutIndexOrAdmin()
     {
         $result = smarty_modifier_prepare_link('products');
-        $expected = 'https://testserver.com/products/index.php'
+        $expected = 'https://testserver.com/products/index.php';
         $this->assertEquals($expected, $result);
     }
 
     public function testPrepareLinkWithLeadingAndTrailingSlashes()
     {
         $result = smarty_modifier_prepare_link('/products/');
-        $expected = 'https://testserver.com/products/index.php'
+        $expected = 'https://testserver.com/products/index.php';
         $this->assertEquals($expected, $result);
     }
 
     public function testPrepareLinkWithSpecialCharacters()
     {
         $result = smarty_modifier_prepare_link('products?name=<script>alert("xss")</script>');
-        $expected = 'https://testserver.com/products/index.php?name=&lt;script&gt;alert(&quot;xss&quot;)&lt;/script&gt;'
+        $expected = 'https://testserver.com/products/index.php?name=&lt;script&gt;alert(&quot;xss&quot;)&lt;/script&gt;';
         $this->assertEquals($expected, $result);
     }
 }
