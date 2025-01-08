43256ea1045937b239c4ff572124073a4d5a1c4c
wilsonify <tom.andrew.wilson@gmail.com>
2025-01-08 11:42:17 -0500

    mcrpyt to openssl

--- c11 - Managing Customer Details/Code/business/symmetric_crypt.php
+++ c11 - Managing Customer Details/Code/business/symmetric_crypt.php
@@ -1,55 +1,53 @@
-<?php
 class SymmetricCrypt
 {
-  // Encryption/decryption key
-  private static $_msSecretKey = 'From Dusk Till Dawn';
+    // Encryption/decryption key
+    private static $_msSecretKey = 'From Dusk Till Dawn';
 
-  // The initialization vector
-  private static $_msHexaIv = 'c7098adc8d6128b5d4b4f7b2fe7f7f05';
+    // The initialization vector (should be 16 bytes for AES-128)
+    private static $_msHexaIv = 'c7098adc8d6128b5d4b4f7b2fe7f7f05';
 
-  // Use the Rijndael Encryption Algorithm
-  private static $_msCipherAlgorithm = MCRYPT_RIJNDAEL_128;
+    // Use AES encryption with CBC mode
+    private static $_msCipherAlgorithm = 'aes-128-cbc';
 
-  /* Function encrypts plain-text string received as parameter
-     and returns the result in hexadecimal format */
-  public static function Encrypt($plainString)
-  {
-    // Pack SymmetricCrypt::_msHexaIv into a binary string
-    $binary_iv = pack('H*', self::$_msHexaIv);
+    /* Function encrypts plain-text string received as parameter
+       and returns the result in hexadecimal format */
+    public static function Encrypt($plainString)
+    {
+        // Pack the IV from hexadecimal to binary
+        $binary_iv = pack('H*', self::$_msHexaIv);
 
-    // Encrypt $plainString
-    $binary_encrypted_string = mcrypt_encrypt(
-                                 self::$_msCipherAlgorithm,
-                                 self::$_msSecretKey,
-                                 $plainString,
-                                 MCRYPT_MODE_CBC,
-                                 $binary_iv);
+        // Encrypt the string using OpenSSL
+        $encrypted_string = openssl_encrypt(
+            $plainString,
+            self::$_msCipherAlgorithm,
+            self::$_msSecretKey,
+            0,
+            $binary_iv
+        );
 
-    // Convert $binary_encrypted_string to hexadecimal format
-    $hexa_encrypted_string = bin2hex($binary_encrypted_string);
+        // Return the encrypted string in hexadecimal format
+        return bin2hex($encrypted_string);
+    }
 
-    return $hexa_encrypted_string;
-  }
+    /* Function decrypts hexadecimal string received as parameter
+       and returns the decrypted result */
+    public static function Decrypt($encryptedString)
+    {
+        // Convert the hexadecimal encrypted string to binary
+        $binary_encrypted_string = hex2bin($encryptedString);
 
-  /* Function decrypts hexadecimal string received as parameter
-     and returns the result in hexadecimal format */
-  public static function Decrypt($encryptedString)
-  {
-    // Pack Symmetric::_msHexaIv into a binary string
-    $binary_iv = pack('H*', self::$_msHexaIv);
+        // Pack the IV from hexadecimal to binary
+        $binary_iv = pack('H*', self::$_msHexaIv);
 
-    // Convert string in hexadecimal to byte array
-    $binary_encrypted_string = pack('H*', $encryptedString);
+        // Decrypt the string using OpenSSL
+        $decrypted_string = openssl_decrypt(
+            $binary_encrypted_string,
+            self::$_msCipherAlgorithm,
+            self::$_msSecretKey,
+            0,
+            $binary_iv
+        );
 
-    // Decrypt $binary_encrypted_string
-    $decrypted_string = mcrypt_decrypt(
-                          self::$_msCipherAlgorithm,
-                          self::$_msSecretKey,
-                          $binary_encrypted_string,
-                          MCRYPT_MODE_CBC,
-                          $binary_iv);
-
-    return $decrypted_string;
-  }
+        return $decrypted_string;
+    }
 }
-
