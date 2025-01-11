e187f36c91efcd19307e1c308738a7bda2fb883a
wilsonify <tom.andrew.wilson@gmail.com>
2025-01-11 16:38:37 -0500

    c16 - Product Reviews - Datacash.com

---
+++ c16 - Product Reviews - Datacash.com/database/01_Create_review_table.sql
@@ -0,0 +1,17 @@
+-- Create review table
+CREATE TABLE review
+(
+  review_id   SERIAL    NOT NULL,
+  customer_id INTEGER   NOT NULL,
+  product_id  INTEGER   NOT NULL,
+  review      TEXT      NOT NULL,
+  rating      SMALLINT  NOT NULL,
+  created_on  TIMESTAMP NOT NULL,
+  CONSTRAINT pk_review_id PRIMARY KEY (review_id),
+  CONSTRAINT fk_customer_id FOREIGN KEY (customer_id)
+             REFERENCES customer (customer_id)
+             ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT fk_product_id FOREIGN KEY (product_id)
+             REFERENCES product (product_id)
+             ON UPDATE RESTRICT ON DELETE RESTRICT
+);
---
+++ c16 - Product Reviews - Datacash.com/database/02_Create_review_info_type.sql
@@ -0,0 +1,8 @@
+-- Create review_info type
+CREATE TYPE review_info AS
+(
+  customer_name VARCHAR(50),
+  review        TEXT,
+  rating        SMALLINT,
+  created_on    TIMESTAMP
+);
